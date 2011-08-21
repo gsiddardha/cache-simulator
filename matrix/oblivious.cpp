ea2ba6a6025ff40e3fd84bd0b35512c97e577a1a
Dinesh Chintanippu <dinesh9920@gmail.com>
2011-08-21 21:56:55 +0530

    Finished matrix/oblivious.cpp

--- matrix/oblivious.cpp
+++ matrix/oblivious.cpp
@@ -3,4 +3,28 @@
 #include <cstdlib>
 #include "../access.h"
 
-void cache_oblivious(Access* accessor) {}
+using namespace std;
+
+void cache_oblivious(void) {
+	int i, j, k;
+
+	recur_func(matrix_n, matrix_m, matrix_p, 0, 0, 0, 0);
+}
+
+void recur_func(int n, int m, int p, int a1, int a2, int b1, int b2){
+	if((n==1) && (m==1) && (p==1)){
+		cout << A(a1, a2)*B(b1, b2) << endl;
+		return;
+	}
+
+	if((n >= m) && (n >= p)) {
+		recur_func(n/2, m, p, a1, a2, b1, b2);
+		recur_func((n - n/2), m, p, a1+n/2, a2, b1, b2);
+	} else if((m >= n) && (m >= p)) {
+		recur_func(n, m/2, p, a1, a2, b1, b2);
+		recur_func(n, (m-m/2), p, a1, a2+m/2, b1+m/2, b2);
+	} else if((p >= m) && (p >= n)) {
+		recur_func(n, m, p/2, a1, a2, b1, b2);
+		recur_func(n, m, (p-p/2), a1, a2, b1, b2+p/2);
+	}
+}
